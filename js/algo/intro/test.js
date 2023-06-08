24947f4b55004bab4d183d2f263cd6f3f58a233a
Sarah Bourgeois <https://github.com/Sarah2511>
2023-06-08 12:12:20 +0200

    exercice 1 2 3 intro

--- js/algo/intro/test.js
+++ js/algo/intro/test.js
@@ -1,1 +1,12 @@
-console.log("It works !!!");
+/*const readlineSync = require("readline-sync");
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);*/
+
+
+let name = "Bourgeois";
+let firstname = "Sarah";
+let city = "Visé";
+
+console.log(firstname);
+console.log(name);
+console.log(city);
