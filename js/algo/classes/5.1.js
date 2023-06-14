27fdb73a4dd973e52ea209ff136f00e6468b9c6e
Sarah Bourgeois <https://github.com/Sarah2511>
2023-06-14 10:15:48 +0200

    exercice class

---
+++ js/algo/classes/5.1.js
@@ -0,0 +1,17 @@
+//initialisation de variables
+
+const readlineSync = require("readline-sync");
+class Person{
+    constructor(firstname, lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    get hello(){
+        return 'Salut ' + this.firstname + ' ' + this.lastname;
+    }
+} 
+let firstname = readlineSync.question('Pouvez-vous entrez votre prénom svp ? ');;
+let lastname =readlineSync.question('Pouvez-vous entrez votre nom de famille svp ? ')
+
+let Sarah = new Person(firstname, lastname);
+console.log(Sarah.hello);
