f0bd9f797605756238ed13e9b39d521d41cadc46
Sarah Bourgeois <https://github.com/Sarah2511>
2023-06-27 15:05:45 +0200

    tout bonus sauf 1

--- montain/asynchrone/exercice2/script.js
+++ montain/asynchrone/exercice2/script.js
@@ -3,6 +3,7 @@ const body = document.querySelector("body");
 const div = document.createElement("div");
 body.appendChild(div);
 const select = document.createElement("select");
+select.setAttribute("id","select");
 body.appendChild(select);
 
 //création d'un bouton permettant de générer des blague aléatoire à partir de l'url d'une api.
@@ -17,7 +18,7 @@ document.querySelector("#button").onclick = function(){
 
     fetch("https://api.chucknorris.io/jokes/categories")
     .then(response => response.json())
-    .then(categories => {
+    .then((categories) => {
         //creation des options du select à partir de l'url 
         for (let i = 0; i < categories.length; i++) {
             let option = document.createElement("option");
@@ -30,11 +31,18 @@ document.querySelector("#button").onclick = function(){
         select.appendChild(option);
 
         //on creer un localstorage pour stocker la valeur au click d'une option.
-        let localstorage = document.createElement("div");
-        document.querySelector("categorie[i]").onclick = localstorage;
-        body.innerHTML = div;
-        body.appendChild(div);
-
+        document.querySelector("#select").onchange = function(e){ 
+            let categorie = e.currentTarget.value;
+            localStorage.setItem("option",categorie);
+            localStorage.getItem("option");
+            console.log(categorie);
+        }
+        
         //on creer un bouton reset pour vider se que contient le local storage creer précédemment.
+
+        document.querySelector("#reset").onclick = function(e){ 
+            let categorie = e.currentTarget.value;
+            localStorage.removeItem("option");
+        }
     })
     .catch(error => console.error(error));
