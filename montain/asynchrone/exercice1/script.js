1df858534b8af6d16c0a8bd72cdd615c6140ff4c
Sarah Bourgeois <https://github.com/Sarah2511>
2023-06-27 14:31:59 +0200

    debut des bonus en version test + ajout de commentaire de mon code :)

--- montain/asynchrone/exercice1/script.js
+++ montain/asynchrone/exercice1/script.js
@@ -1,6 +1,8 @@
+//initialisation de variable
 const body = document.querySelector("body");
 const ul = document.createElement("ul");
 body.appendChild(ul);
+//création d'un bouton permettant de creer une liste dont les données sont stocker dans un fichier .json. 
 document.querySelector("#button").onclick = function(){ 
     fetch("becode.json").then(response => response.json()).then(data => {
         data.forEach(ligne => {
--- montain/asynchrone/exercice2/script.js
+++ montain/asynchrone/exercice2/script.js
@@ -1,10 +1,40 @@
+//initialisation de variable
 const body = document.querySelector("body");
 const div = document.createElement("div");
 body.appendChild(div);
+const select = document.createElement("select");
+body.appendChild(select);
+
+//création d'un bouton permettant de générer des blague aléatoire à partir de l'url d'une api.
 document.querySelector("#button").onclick = function(){ 
     fetch("https://api.chucknorris.io/jokes/random")
     .then(response => response.json()).then(data => {
         div.innerHTML = data.value;
         console.log(data.value);
     })
+    .catch(error => console.error(error));
 };
+
+    fetch("https://api.chucknorris.io/jokes/categories")
+    .then(response => response.json())
+    .then(categories => {
+        //creation des options du select à partir de l'url 
+        for (let i = 0; i < categories.length; i++) {
+            let option = document.createElement("option");
+            option.innerText = categories[i];
+            select.appendChild(option);
+        }
+        //on cree une option autre a la fin des autres options
+        const option = document.createElement("option");
+        option.innerHTML = "autre...";
+        select.appendChild(option);
+
+        //on creer un localstorage pour stocker la valeur au click d'une option.
+        let localstorage = document.createElement("div");
+        document.querySelector("categorie[i]").onclick = localstorage;
+        body.innerHTML = div;
+        body.appendChild(div);
+
+        //on creer un bouton reset pour vider se que contient le local storage creer précédemment.
+    })
+    .catch(error => console.error(error));
