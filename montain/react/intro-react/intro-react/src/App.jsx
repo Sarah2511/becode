c1ed5fb1671ede3fda1229c9411a1c3f28ce00db
Sarah Bourgeois <https://github.com/Sarah2511>
2023-07-10 12:32:25 +0200

    réalisation de la to do list mais pas encore de css

--- montain/react/intro-react/intro-react/src/App.jsx
+++ montain/react/intro-react/intro-react/src/App.jsx
@@ -2,24 +2,44 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
 
   return (
-    <>
-        <div className='header-todo'>
-          <h1>To Do :</h1>
-
-          <div className='add-task-content'>
-            <input type="text" />
-            <button className='button-add-task'>Add Task</button>
-          </div>
-
-          <ul className="todo-list">
-
-          </ul>
-        </div>
-    </>
-  )
+    <div>
+      <h1>To Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={task} onChange={handleChange} />
+        <button type="submit">Ajouter</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleDelete(index)}>Supprimer</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
